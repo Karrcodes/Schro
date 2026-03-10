5496ac8bdfb2bcf4554b9e37b366f16401ae40b0
Karr <127051401+Karrcodes@users.noreply.github.com>
2026-03-10 10:08:53 +0000

    .

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    experimental: {
+        // @ts-ignore
+        allowedDevOrigins: ['192.168.0.27', 'localhost:3000']
+    }
+};
 
 export default nextConfig;
