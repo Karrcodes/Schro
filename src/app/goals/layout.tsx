fa067326605a295b65f182d39893073307f53a6a
Karr <127051401+Karrcodes@users.noreply.github.com>
2026-03-18 20:03:52 +0000

    .

--- src/app/goals/layout.tsx
+++ src/app/goals/layout.tsx
@@ -2,22 +2,25 @@
 
 import * as React from 'react'
 import { cn } from '@/lib/utils'
+import { GoalsProvider } from '@/features/goals/contexts/GoalsContext'
 import { KarrFooter } from '@/components/KarrFooter'
 
 export default function GoalsLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="bg-[#FAFAFA] flex flex-col min-h-screen">
-            <div className="flex-1 overflow-y-auto bg-[#fafafa]">
-                {/* Tab Content */}
-                <div className="p-6 md:p-10">
-                    <div className="w-full h-full flex flex-col max-w-7xl mx-auto space-y-12">
-                        {children}
-                        <div className="mt-auto pt-10">
-                            <KarrFooter />
+        <GoalsProvider>
+            <div className="bg-[#FAFAFA] flex flex-col min-h-screen">
+                <div className="flex-1 overflow-y-auto bg-[#fafafa]">
+                    {/* Tab Content */}
+                    <div className="p-6 md:p-10">
+                        <div className="w-full h-full flex flex-col max-w-7xl mx-auto space-y-12">
+                            {children}
+                            <div className="mt-auto pt-10">
+                                <KarrFooter />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </GoalsProvider>
     )
 }
